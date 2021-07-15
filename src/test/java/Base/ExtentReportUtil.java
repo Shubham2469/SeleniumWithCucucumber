e881df9a8d14e145a97e1ac94a7e6ca256f55b7f
Shubham2469 <66327581+Shubham2469@users.noreply.github.com>
2021-07-15 20:33:22 +0530

    Update ExtentReportUtil.java

--- src/test/java/Base/ExtentReportUtil.java
+++ src/test/java/Base/ExtentReportUtil.java
@@ -1,7 +1,7 @@
 package Base;
 
 import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 /**
- * Created by Karthik on 21/09/2019.
+ * Created by Karthik on 31/01/2019.
  */
 
 
@@ -24,15 +24,19 @@ public class ExtentReportUtil extends BaseUtil {
         //First is to create Extent Reports
         extent = new ExtentReports();
 
-        ExtentSparkReporter spark = new ExtentSparkReporter(fileName);
+        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(fileName);
+        htmlReporter.config().setTheme(Theme.DARK);
+        htmlReporter.config().setDocumentTitle("Test report for Selenium Basic");
+        htmlReporter.config().setEncoding("utf-8");
+        htmlReporter.config().setReportName("Test report");
 
-        extent.attachReporter(spark);
+        extent.attachReporter(htmlReporter);
 
     }
 
     public void ExtentReportScreenshot() throws IOException {
 
-        var scr = ((TakesScreenshot)Driver).getScreenshotAs(OutputType.FILE);
+        File scr = ((TakesScreenshot)Driver).getScreenshotAs(OutputType.FILE);
         Files.copy(scr.toPath(), new File(reportLocation + "screenshot.png").toPath());
         scenarioDef.fail("details").addScreenCaptureFromPath(reportLocation + "screenshot.png");
     }
